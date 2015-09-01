17f7b7a03d9ade034883c2ce987e55c721162b06
Jason Desiderio <jmdesiderio@gmail.com>
2015-08-31 21:37:35 -0400

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Twitterish"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title", "Help | Twitterish"
+  end
+
+  test "should get about" do
+  	get :about
+  	assert_response :success
+    assert_select "title", "About | Twitterish"
   end
 
 end
